59b30b31c7d0832f39f214d694cad2c84e7d97e3
zbl14 <ifthereisoneday@gmail.com>
2022-07-29 08:41:31 -0700

    set up client and stylist model

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -2,6 +2,9 @@ namespace HairSalon.Models
 {
   public class Client
   {
-    // properties, constructors, methods, etc. go here
+    public int ClientId { get; set; }
+    public string Name { get; set; }
+    public int StylistId { get; set; }
+    public virtual Stylist Stylist { get; set; }
   }
 }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -2,6 +2,13 @@ namespace HairSalon.Models
 {
   public class Stylist
   {
-    // properties, constructors, methods, etc. go here
+    public Stylist()
+    {
+        this.Clients = new HashSet<Client>();
+    }
+
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
   }
 }
