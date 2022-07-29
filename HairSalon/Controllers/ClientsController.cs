6776458d36316055b3088f30365911152a0063eb
zbl14 <ifthereisoneday@gmail.com>
2022-07-29 11:04:54 -0700

    add Create() and Details(), update Index()

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -18,9 +18,30 @@ namespace HairSalon.Controllers
 
     public ActionResult Index()
     {
-      List<Client> model = _db.Clients.ToList();
+      List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();
       ViewBag.PageTitle = "View All Clients";
       return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.PageTitle = "New Client";
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
     }
   }
 }
