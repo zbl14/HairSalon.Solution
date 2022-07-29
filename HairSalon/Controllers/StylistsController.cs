f23a4063063b55ba73dcac9ce045d8ebf256dc96
zbl14 <ifthereisoneday@gmail.com>
2022-07-29 10:04:06 -0700

    add Create()

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -21,5 +21,19 @@ namespace HairSalon.Controllers
       ViewBag.PageTitle = "View All Stylists";
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      ViewBag.PageTitle = "New Stylist";
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Stylist stylist)
+    {
+      _db.Stylists.Add(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
