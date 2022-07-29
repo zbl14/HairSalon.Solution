41e98f115f91f7ec9d7bf98d502f7541d41c361c
zbl14 <ifthereisoneday@gmail.com>
2022-07-29 09:46:42 -0700

    add Index()

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -8,6 +8,18 @@ namespace HairSalon.Controllers
 {
   public class StylistsController : Controller
   {
+    private readonly HairSalonContext _db;
 
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      ViewBag.PageTitle = "View All Stylists";
+      return View(model);
+    }
   }
 }
